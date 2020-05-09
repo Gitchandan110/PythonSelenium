5f1f7a52a34b0a8ab7032f6945132bb74af87f9e
chandan kumar <chandan.kumar@netsutra.com>
2020-05-09 18:17:41 +0530

    cu

---
+++ src/SK_HomePage.py
@@ -0,0 +1,19 @@
+'''
+Created on May 9, 2020
+
+@author: Chandan
+'''
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+
+url="https://safety-red5.kuvrr.com/?next=/observer/"
+
+from selenium import webdriver
+
+driver = webdriver.Chrome(executable_path= 'C:\\Users\\Chandan\\Git\\chromedriver.exe')
+
+driver.get(url)
+print("Please Login ")
+element=driver.find_element_by_xpath("//input[@id='email'][@name='email']")
+element.send_keys("lackorg@yopmail.com")
+
